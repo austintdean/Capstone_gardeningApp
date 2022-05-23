fab8eb13cc7836da711b01a465a71b0e464ea7c7
Austin <ofthevalley111@gmail.com>
2022-05-23 11:15:11 -0500

    created plant model + registered Plant in Installed App

--- backend/plants/models.py
+++ backend/plants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255)
+    seed_depth = models.IntegerField()
+    seed_spacing = models.IntegerField()
+    water_cycle = models.CharField(max_length=255)
+    water_amount = models.CharField(max_length=255)
+    time_to_harvest = models.DateField()
+    harvested = models.BooleanField(default=False)
+    season = models.CharField(max_length=255)
